f4e273ca3f9abd1fcfed0f01f509679bf98f1d35
anilnlr <30968843+anilnlr@users.noreply.github.com>
2017-08-15 21:28:58 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,6 @@ var element = document.getElementById('center-text');
 element.innerText = "New Value";
 
 var img = document.getElementById('madi');
-img.onClick = function(){
+img.onclick = function(){
 img.style.marginLeft = '100px';
 };
