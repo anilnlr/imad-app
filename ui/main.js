5f45b0e2c3479f823e48dcf866dd4bf18bce8c00
anilnlr <30968843+anilnlr@users.noreply.github.com>
2017-08-18 13:47:35 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,27 @@
-console.log('Loaded!');
-var element = document.getElementById('center-text');
-element.innerText = "New Value";
+//console.log('Loaded!');
+//var element = document.getElementById('center-text');
+//element.innerText = "New Value";
 
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft + 10;
-    img.style.marginLeft = marginLeft + 'px';
-}
+//var img = document.getElementById('madi');
+//var marginLeft = 0;
+//function moveRight(){
+//    marginLeft = marginLeft + 10;
+//    img.style.marginLeft = marginLeft + 'px';
+//}
 
+//img.onclick = function(){
+//    var interval = setInterval(moveRight, 100);
+//};
 
-img.onclick = function(){
-    var interval = setInterval(moveRight, 100);
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function(){
+    // Make a request to teh counter end point
+    
+    // capture the response and store it in a variable
+    
+    // Render the variable in the correct span
+    counter = counter+1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
 };
