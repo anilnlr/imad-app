b35ab96c771340f2c0683347f7c013de6e4be889
anilnlr <30968843+anilnlr@users.noreply.github.com>
2017-08-27 22:21:43 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user: 'anilnlr1983',
+    database : 'anilnlr1983'
+    host : 'db.imad.hasura-app.io',
+    port : '5432',
+    password : process.env.DB_PASSWORD
+}
 
 var app = express();
 app.use(morgan('combined'));
@@ -92,6 +101,18 @@ app.get('/', function (req, res) {
 //app.get('/article-three', function (req, res) {
 //  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 //});
+
+var pool = new Pool(config);
+app.get('/test-db', function(req,res) {
+    pool.query('SELECT * FROM test', function(err,result)) {
+        if(err) {
+            res.status(500).send(err.toString());
+        }
+        else {
+            res.send(JSON.stringify(result));
+        }
+    }
+});
 
 //var counter = 0;
 app.get('/counter', function (req, res) {
